dbd530d73c4c38eafa74206df857c59ba80abdb9
DESKTOP-HVCI546\a <237964@student.pwr.edu.pl>
2018-09-01 10:36:16 +0200

    [#1.1]

--- DevmeetingsProject/App.js
+++ DevmeetingsProject/App.js
@@ -1,23 +1,59 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { Constants } from 'expo';
 
 export default class App extends React.Component {
+  state = {
+    items:
+    [
+      {
+        title: `Zadanie 1`,
+        key: 1,
+        content: `Zrobić zakupy`,
+      }
+    ]
+    // items: new Array(3).fill(0).map((a, i) => i).map(i => ({
+    //   title: `Title ${i}`,
+    //   key: i,
+    //   content: `Content number ${i}. It's a bit longer than title. It's even long enough to force a line break`,
+    // })),
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text style={styles.title}>Lista zadań na dzisiaj</Text>
+        <FlatList data={this.state.items} renderItem={this.renderItem} />
       </View>
     );
   }
+
+  renderItem = ({ item }) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{item.title}</Text>
+      <Text style={styles.content}>{item.content}</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 0,
+    margin: 0,
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  },
+  item: {
+    paddingHorizontal: 10,
+  },
+  title: {
+    fontWeight: 'bold',
+    marginVertical: 5,
+  },
+  content: {
+    marginBottom: 10,
   },
 });
